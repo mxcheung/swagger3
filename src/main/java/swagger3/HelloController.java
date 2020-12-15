4a56234daf861842e5c9f35497ac62cde5db38b6
mxcheung <mxcheung@users.noreply.github.com>
2020-12-15 22:40:19 +1100

    Update HelloController.java

--- src/main/java/swagger3/HelloController.java
+++ src/main/java/swagger3/HelloController.java
@@ -3,11 +3,16 @@ package swagger3;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import lombok.extern.slf4j.Slf4j;
+
+
 @RestController
+@Slf4j
 public class HelloController {
 
     @RequestMapping("/")
     public String index() {
+        log.info("getGreeting message {} ",  "hi");
         return "Greetings from Spring Boot!";
     }
 
